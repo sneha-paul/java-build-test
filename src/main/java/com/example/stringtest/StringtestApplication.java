1212e559af3bfeae2bb863b5abc529367ce10076
sneha-paul <sneha.paul@e-xposures.com>
2021-11-25 10:22:34 +0530

    string commits1

--- src/main/java/com/example/stringtest/StringtestApplication.java
+++ src/main/java/com/example/stringtest/StringtestApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StringtestApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(StringtestApplication.class, args);
 	}
